c7c32ab3502caa55f918f8a62fa9f07d1e8dd528
Arnold Veigas <67996677+Arnold17V@users.noreply.github.com>
2022-05-01 23:24:03 +0530

    Create kann_valid.py

---
+++ kann_valid.py
@@ -0,0 +1,19 @@
+import re
+
+def KannOrNot(word):
+  rangeStart = ur"\u0C80"
+  rangeEnd = ur"\u0CFF"
+  pattern = rangeStart + '-' + rangeEnd
+    if re.match('^[' + pattern + ']+$',word) != None:
+        return True
+    else:
+        return False
+
+#input
+myString = u(input())
+wordsList = myString.split()
+for eachWord in wordsList:
+  if KannOrNot(eachWord):
+    NLProject()    
+  else:
+    print("Error! Try to enter only words in the Kannada dialect")
